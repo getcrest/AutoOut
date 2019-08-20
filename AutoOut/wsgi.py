0917062690a5ca5b16407e92d9de728e9f1604d8
kailash <ahikailash1@gmail.com>
2019-08-20 18:14:33 +0530

    Dynamic Dask cluster creation enabled

--- AutoOut/wsgi.py
+++ AutoOut/wsgi.py
@@ -7,6 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
+import math
+import multiprocessing
 import os
 
 from distributed import LocalCluster, Client
@@ -15,7 +17,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AutoOut.settings')
 
 # Start dask cluster
-c = LocalCluster(processes=False, n_workers=2, threads_per_worker=3)
+no_cpus = multiprocessing.cpu_count()
+threads_per_worker = 2
+no_workers = math.floor((no_cpus-1)/threads_per_worker)
+
+c = LocalCluster(processes=False, n_workers=no_workers, threads_per_worker=threads_per_worker)
 dask_client = Client(c)
 
 application = get_wsgi_application()
