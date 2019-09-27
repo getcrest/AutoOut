77c01db02a79fdd16828ecf6d14518d2b4b5ee34
kailash <ahikailash1@gmail.com>
2019-09-27 13:01:15 +0530

    Bug fix: Dask cluster creation related bug fixed

--- AutoOut/wsgi.py
+++ AutoOut/wsgi.py
@@ -13,15 +13,19 @@ import os
 
 from distributed import LocalCluster, Client
 from django.core.wsgi import get_wsgi_application
+from psutil import virtual_memory
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AutoOut.settings')
 
 # Start dask cluster
 no_cpus = multiprocessing.cpu_count()
 threads_per_worker = 2
-no_workers = math.floor((no_cpus-1)/threads_per_worker)
+no_workers = math.floor((no_cpus-2)/threads_per_worker)
 
-c = LocalCluster(processes=False, n_workers=no_workers, threads_per_worker=threads_per_worker)
+mem = virtual_memory()
+
+c = LocalCluster(processes=False, n_workers=no_workers, threads_per_worker=threads_per_worker,
+                 memory_limit=mem.free/no_workers)
 dask_client = Client(c)
 
 application = get_wsgi_application()
